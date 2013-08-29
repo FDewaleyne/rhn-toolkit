5652d46c372b35f38f3b50e104a032388a74e77f
Felix Dewaleyne <fdewaley@redhat.com>
2013-08-29 10:31:55 +0100

    syncing with work done previously

--- rhn-export-to-csv.py
+++ rhn-export-to-csv.py
@@ -11,13 +11,13 @@
 __author__ = "Felix Dewaleyne"
 __credits__ = ["Felix Dewaleyne"]
 __license__ = "GPL"
-__version__ = "0.1"
+__version__ = "0.3"
 __maintainer__ = "Felix Dewaleyne"
 __email__ = "fdewaley@redhat.com"
 __status__ = "prod"
 
 
-import xmlrpclib, sys
+import xmlrpclib, sys, getpass
 #connection part
 client = xmlrpclib.Server("https://rhn.redhat.com/rpc/api")
 sys.stderr.write("enter your RHN login: ")
@@ -35,22 +35,22 @@ for system in client.system.listUserSystems(key):
     #gather the details for each system
     rhn_data[system['id']] = {'id': system['id'], 'last_checkin': system['last_checkin']}
     #add all the data from getDetails to this dictionary
-    rhn_data[system['id']].update(client.system.getDetails(key,system['id']))
+    rhn_data[system['id']].update(client.system.getDetails(key,int(system['id'])))
     #add all the network details as well
-    rhn_data[system['id']].update(client.system.getNetwork(key,system['id']))
+    rhn_data[system['id']].update(client.system.getNetwork(key,int(system['id'])))
 
-client.auth.logout()
+client.auth.logout(key)
 #now write the csv file
 print "Writing data to the csv file"
-headers={'ID':'id' , 'Profile name':'profile_name', 'Hostname':'hostname', 'IP':'ip', 'Last checkin':'last_checkin', 'Base Entitlement': 'base_entitlement', 'Description': 'description', 'Adress 1': 'adress1','Adress 2':'adress2', 'City':'city', 'State': 'state', 'Country':'country', 'Building':'building', 'Room':'room', 'Rack':'rack'}
+headers=['id' , 'profile_name', 'hostname', 'ip', 'last_checkin', 'base_entitlement', 'description', 'adress1','adress2', 'city', 'state', 'country', 'building', 'room', 'rack']
 import csv
-with open('systems_'+login+'.csv', 'wb' ) as csvfile:
-    csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-    csv_writer.writerow(headers.keys())
-    for system in rhn_data:
-        line = ()
-        for value in headers:
-            line.append(system.get(value))
-        csv_writer.writerow(line)
-        del line
+csvfile = open('systems_'+login+'.csv', 'wb' )
+csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+csv_writer.writerow(headers)
+for system in rhn_data:
+    line = []
+    for value in headers:
+        line.append(system.get(value))
+    csv_writer.writerow(line)
+    del line
 del rhn_data
